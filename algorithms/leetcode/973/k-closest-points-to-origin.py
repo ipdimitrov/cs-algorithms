3c969b76ee9bb6384ad862713e9315c5af2f18db
ipdimitrov <ivaylo.dimitrov@protonmail.com>
2025-07-01 17:11:13 +0000

    703 & 973

---
+++ algorithms/leetcode/973/k-closest-points-to-origin.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key = lambda p: p[0]**2 + p[1]**2)
+        return points[:k]
